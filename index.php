e7e993a41804b6de7985e2d93a4b4b7389602818
RaniFerreira <franielly535@gmail.com>
2024-03-26 15:15:38 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Poupe Bem</title>
+    <link rel="stylesheet" href="src/css/style.css">
+</head>
+<body>
+
+<header>
+        <nav class="navbar">
+            <div class="container">
+                <a href="#" class="logo">
+                    <img src="src/img/logo-removebg-preview.png" alt="Logo">
+                </a>
+                <ul class="nav-links">
+                    <li><a href="#Home">HOME</a></li>
+                    <li><a href="#encomendas">ENCOMENDAS</a></li>
+                    <li><a href="#fomulario">FORMULÁRIO</a></li>
+                    <li><a href="#Administrador">ADMINISTRADOR</a></li>
+                    <li><a href="#cadastro">CADASTRO</a></li>
+            </div>
+        </nav>
+    </header>
+    
+</body>
+</html>
