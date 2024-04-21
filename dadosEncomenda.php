3ac3d3d9ccf14b388f4758f32fb14569b938f603
RaniFerreira <franielly535@gmail.com>
2024-04-21 16:35:06 -0300

    identando

--- dadosEncomenda.php
+++ dadosEncomenda.php
@@ -1,17 +1,17 @@
 <?php
-    include_once('conexaoBD.php');
-    $nome = $_POST['nome'];
-    $telefone = $_POST['telefone'];
-    $curso = $_POST['curso'];
-    
-    
+include_once ('conexaoBD.php');
+$nome = $_POST['nome'];
+$telefone = $_POST['telefone'];
+$curso = $_POST['curso'];
 
 
-    $mgs_encomend = "INSERT INTO encomenda(nome,telefone,curso) VALUES ('$nome','$telefone','$curso')";
 
-    mysqli_query($conn,$mgs_encomend);
 
-    header("Location: index.php");
-        exit;
+$mgs_encomend = "INSERT INTO encomenda(nome,telefone,curso) VALUES ('$nome','$telefone','$curso')";
+
+mysqli_query($conn, $mgs_encomend);
+
+header("Location: index.php");
+exit;
 
 ?>
--- dadosFormulario.php
+++ dadosFormulario.php
@@ -1,19 +1,19 @@
 <?php
-    include_once('conexaoBD.php');
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $telefone = $_POST['telefone'];
-    $genero = $_POST['genero'];
-    $mensagem = $_POST['mensagem'];
-    
-    
+include_once ('conexaoBD.php');
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$genero = $_POST['genero'];
+$mensagem = $_POST['mensagem'];
 
 
-    $mgs_formulario = "INSERT INTO formularioduvida(nome,email,telefone,genero,mensagem) VALUES ('$nome','$email','$telefone','$genero','$mensagem')";
 
-    mysqli_query($conn,$mgs_formulario);
 
-    header("Location: index.php");
-        exit;
+$mgs_formulario = "INSERT INTO formularioduvida(nome,email,telefone,genero,mensagem) VALUES ('$nome','$email','$telefone','$genero','$mensagem')";
+
+mysqli_query($conn, $mgs_formulario);
+
+header("Location: index.php");
+exit;
 
 ?>
